33f346ca068e477da832b08d5143c8c53611684e
asier <asier.ayoga@elorrieta-errekamari.com>
2021-11-06 13:43:55 +0100

    dialogo show

---
+++ Proyecto/app/src/main/java/com/example/proyecto/Dialogo.java
@@ -0,0 +1,15 @@
+package com.example.proyecto;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dialogo extends AppCompatActivity {
+}
--- Proyecto/app/src/main/java/com/example/proyecto/mainMenu.java
+++ Proyecto/app/src/main/java/com/example/proyecto/mainMenu.java
@@ -1,19 +1,29 @@
 package com.example.proyecto;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class mainMenu extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
     }
 
     @Override
@@ -25,6 +35,7 @@ public class mainMenu extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id==R.id.op1) {
+            mostrarDialoo();
             Toast.makeText(this,"Se seleccionó la primer opción",Toast.LENGTH_LONG).show();
         }
         if (id==R.id.op2) {
@@ -48,6 +59,33 @@ public class mainMenu extends AppCompatActivity {
         startActivity(i);
     }
 
+    public void mostrarDialoo(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(mainMenu.this);
 
+        LayoutInflater inflater = getLayoutInflater();
 
+        View view = inflater.inflate(R.layout.activity_dialogo,null);
+
+        builder.setView(view);
+
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+
+        Button aceptar = view.findViewById(R.id.btnaceptar);
+        aceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText viejo = findViewById(R.id.etviejo);
+                String contraviejo = viejo.getText().toString();
+                SharedPreferences prefe=getSharedPreferences("login", Context.MODE_PRIVATE);
+                String c=prefe.getString(contraviejo, "");
+
+                if (c.length()==0){
+                    Toast.makeText(getApplicationContext(),"No existe dicho nombre", Toast.LENGTH_LONG).show();
+                }
+                dialog.dismiss();
+            }
+        });
+
+    }
 }
